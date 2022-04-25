b3b76c3f0d08eb5f083b0cd48ed06b59419cac01
Italo Gabriel <italog03@gmail.com>
2022-04-25 14:36:10 -0300

    feat: add pagination for projects

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -3,15 +3,24 @@ const knex = require("../database");
 module.exports = {
   async index(req, res) {
     try {
-      const { user_id } = req.query;
-      const query = knex("projects");
+      const { user_id, page = 1 } = req.query;
+      const query = knex("projects")
+        .limit(5)
+        .offset((page - 1) * 5);
+
+      const countObj = knex("projects").count();
 
       if (user_id) {
         query
           .where({ user_id })
           .join("users", "users.id", "=", "projects.user_id")
           .select("projects.*", "users.username");
+
+        countObj.where({ user_id });
       }
+
+      const [count] = await countObj
+      res.header("X-Total-Count", count["count"]);
 
       const projects = await query;
       return res.status(200).json(projects);
