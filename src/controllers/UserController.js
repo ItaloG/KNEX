e2dfcc17ed98256ca7ccb8d9176ab89944ba3118
Italo Gabriel <italog03@gmail.com>
2022-04-25 10:14:39 -0300

    feat: add create user route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,17 @@
-const knex = require('../database')
+const knex = require("../database");
 
 module.exports = {
   async index(req, res) {
     const users = await knex("users");
     return res.status(200).json(users);
+  },
+  async create(req, res, next) {
+    try {
+      const { username } = req.body;
+      await knex("users").insert({username});
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
   },
 };
