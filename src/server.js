880c6c5739a4c57ddf914b98336e80e19bd80655
Italo Gabriel <italog03@gmail.com>
2022-04-25 13:25:53 -0300

    feat: show message if route not found

--- src/server.js
+++ src/server.js
@@ -6,6 +6,12 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use((req, res, next) => {
+    const error = new Error('not found')
+    error.status = 404
+    next(error)
+})
+
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({ error: error.message })
 })
