0f77fd3bbe5d46ed5320f408ea0d07b9e214afac
Italo Gabriel <italog03@gmail.com>
2022-04-25 10:09:10 -0300

    feat: add middleware

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ const routes = require('./routes')
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({ error: error.message })
+})
 
 app.listen(3333, () => console.log('Server is running in http://localhost:3333'))
